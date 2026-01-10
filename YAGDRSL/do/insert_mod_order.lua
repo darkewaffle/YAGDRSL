c4c05f142c15780ee9caf58214fc7521ce000397
darkwaffle <darkewaffle@gmail.com>
2026-01-10 02:39:39 -0500

    Refactor of SetModOrder and AppendModOrder to support priority values. Append now only accepts a single modname as well.

--- YAGDRSL/do/insert_mod_order.lua
+++ YAGDRSL/do/insert_mod_order.lua
@@ -1,10 +1,7 @@
 require "YAGDRSL/get/mods/get_mod_exists.lua"
 
-function InsertIntoModOrder(ModOrderName, ResetList, ...)
-
-	if ResetList then
-		_G[MOD_ORDER_CONTAINER][ModOrderName] = {}
-	end
+function SetModOrder(ModOrderName, ...)
+	_G[MOD_ORDER_CONTAINER][ModOrderName] = {}
 
 	local ModOrderList = _G[MOD_ORDER_CONTAINER][ModOrderName]
 	local ModsToInsert = {...}
@@ -18,31 +15,51 @@ function InsertIntoModOrder(ModOrderName, ResetList, ...)
 	end
 end
 
-function SetModOrder(ModOrderName, ...)
-	InsertIntoModOrder(ModOrderName, true, ...)
+function AppendModOrder(ModOrderName, ModName, Priority)
+	local ModOrderList = _G[MOD_ORDER_CONTAINER][ModOrderName]
+	local Position = 0
+
+	ChatNotice(ModOrderName .. " " .. ModName, Priority)
+
+	if GetModExists(ModName) then
+
+		if Priority == MOD_ORDER_PRIORITY_LOW then
+			-- Puts the mod at the start of the order which will make it low priority
+			Position = 1
+		else
+			-- Puts the mod at the end of the order which will make it high priority
+			Position = nil
+		end
+
+		if Position and Position > 0 then
+			table.insert(ModOrderList, Position, ModName)
+		else
+			table.insert(ModOrderList, ModName)
+		end
+
+	else
+		ChatError("Not Added To Mod Order(s) -", ModName)
+	end
+
 end
 
-function AppendModOrder(ModOrderName, ...)
-	InsertIntoModOrder(ModOrderName, false, ...)
-end
-
-function AppendModOrderToOrderList(OrderList, ...)
+function AppendModOrderToOrderList(OrderList, ModName, Priority)
 	for _, ModOrderName in ipairs(OrderList) do
-		AppendModOrder(ModOrderName, ...)
+		AppendModOrder(ModOrderName, ModName, Priority)
 	end
 end
 
-function AppendModOrderOffense(...)
+function AppendModOrderOffense(ModName, Priority)
 	local ModOrderList = GetModOrderListOffense()
-	AppendModOrderToOrderList(ModOrderList, ...)
+	AppendModOrderToOrderList(ModOrderList, ModName, Priority)
 end
 
-function AppendModOrderDefense(...)
+function AppendModOrderDefense(ModName, Priority)
 	local ModOrderList = GetModOrderListDefense()
-	AppendModOrderToOrderList(ModOrderList, ...)
+	AppendModOrderToOrderList(ModOrderList, ModName, Priority)
 end
 
-function AppendModOrderClass(ModClass, ...)
+function AppendModOrderClass(ModClass, ModName, Priority)
 	local ModOrderList = GetModOrderList(ModClass)
-	AppendModOrderToOrderList(ModOrderList, ...)
+	AppendModOrderToOrderList(ModOrderList, ModName, Priority)
 end
