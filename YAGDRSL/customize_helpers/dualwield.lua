e338b9584faa770bbd51daaf20f36b804386e567
darkwaffle <darkewaffle@gmail.com>
2026-01-23 20:46:45 -0500

    CustomizeDualWield documentation comments

--- YAGDRSL/customize_helpers/dualwield.lua
+++ YAGDRSL/customize_helpers/dualwield.lua
@@ -1,6 +1,37 @@
 require "YAGDRSL/get/get_dualwield.lua"
 require "YAGDRSL/get/get_haste.lua"
 
+--[[
+
+	@User
+	This function can be used to select a set of DualWield equipment based off a specified amount.
+	Recommended to make use of GetCharacterHaste and GetDualWieldToCap to calculate DualWieldToAdd.
+	This function will search for sets in the DualWield table. The keys should correspond to the amount of DualWield the set provides. See below for an example.
+	CustomizeDualWield will select the set that provides the most dual wield <= DualWieldtoAdd and combine it into GearSet.
+
+	DualWield = {}
+	DualWield[0] = {}
+
+	DualWield[5] =
+		{
+			ear1="Suppanomimi"
+		}
+
+	DualWield[10] =
+		{
+			ear1="Suppanomimi",
+			waist="Patentia Sash"
+		}
+
+	DualWield[14] =
+		{
+			ear1="Suppanomimi",
+			ear2="Eabani Earring",
+			waist="Patentia Sash"
+		}
+
+]]
+
 function CustomizeDualWield(GearSet, DualWieldToAdd)
 	local DualWieldSetAmounts = {}
 	local SelectedDualWieldSet = 0
