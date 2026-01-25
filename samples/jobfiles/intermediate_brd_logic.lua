ec67aef328267e5cbde57c0a71048f5b56bb6879
darkwaffle <darkewaffle@gmail.com>
2026-01-25 01:58:08 -0500

    Bard HookPrecastTerminate implementation to make use of ScaleCure

--- samples/jobfiles/intermediate_brd_logic.lua
+++ samples/jobfiles/intermediate_brd_logic.lua
@@ -9,3 +9,15 @@ TrackOffenseBuffs("Troubadour")
 
 AssignWeaponLockOptions(MOD_LOCK_ALL, MOD_LOCK_MAIN_SUB, MOD_LOCK_MAIN_SUB_AMMO)
 SetWeaponLockMainSubAmmo()
+
+
+function HookPrecastTerminate(SpellAttributes)
+	local TerminateSpell = false
+	local TerminateReason = "Spell will not be terminated"
+
+	if SpellAttributes["ParentGroup"] == MAP_SPELL_CURE then
+		TerminateSpell, TerminateReason = ScaleCure(SpellAttributes, 1.5, 1, 1)
+	end
+
+	return TerminateSpell, TerminateReason
+end
