dd73ad155579dbb37e32d138d80de57024c1a078
darkwaffle <darkewaffle@gmail.com>
2026-02-03 20:25:37 -0500

    PrecastTerminateAilments list of ailment definitions expanded

--- YAGDRSL/events/gearswap/terminations/terminate_precast.lua
+++ YAGDRSL/events/gearswap/terminations/terminate_precast.lua
@@ -43,13 +43,20 @@ function PrecastTerminateAilments(SpellAttributes)
 	local Ailments =
 	{
 		[0] = AllActions,									-- KO / dead
+		[2] = AllActions,									-- Sleep
 		[6] = {[CATEGORY_MAGIC]=true},						-- Silence
 		[7] = AllActions,									-- Petrify
 		[10] = AllActions,									-- Stun
 		[14] = AllActions,									-- Charm
-		[17] = AllActions,									-- Charm
 		[16] = {[CATEGORY_JA]=true, [CATEGORY_WS]=true},	-- Amnesia
-		[28] = AllActions									-- Terror
+		[17] = AllActions,									-- Charm
+		[19] = AllActions,									-- Sleep
+		[28] = AllActions,									-- Terror
+		[29] = {[CATEGORY_MAGIC]=true},						-- Mute
+		[193] = AllActions,									-- Lullaby
+		[261] = {[CATEGORY_JA]=true, [CATEGORY_WS]=true},	-- Impairment (Nyzul)
+		[262] = {[CATEGORY_MAGIC]=true},					-- Omerta (Nyzul)
+		[633] = AllActions									-- Animated (Berserk'd)
 	}
 
 	local PlayerBuffs = GetCharacterBuffs()
