fe53a66a4a274f0d869ef17f5179866eec0ec100
darkwaffle <darkewaffle@gmail.com>
2026-02-05 16:39:06 -0500

    SelfUpdateCommand updated to read CommandInputs to identify source of the call and whether the update should ignore weapon lock or not

--- YAGDRSL/events/gearswap/commands/update.lua
+++ YAGDRSL/events/gearswap/commands/update.lua
@@ -1,3 +1,12 @@
 function SelfCommandUpdate(CommandInputs)
-	ForceStatusUpdate(EVENT_SELF_COMMAND_UPDATE)
+	local MapStringToBoolean =
+	{
+		["true"] = true,
+		["false"] = false
+	}
+
+	local UpdateSource = CommandInputs[1] or EVENT_SELF_COMMAND_UPDATE
+	local IgnoreLock = MapStringToBoolean[CommandInputs[2]] or false
+
+	ForceStatusUpdate(UpdateSource, IgnoreLock)
 end
