52b6c3a2686cd3dda3fe9480c1960afc2e8d7710
darkwaffle <darkewaffle@gmail.com>
2026-02-08 22:24:00 -0500

    OnChunk support functions specific to processing certain types of packets and actions

---
+++ YAGDRSL/events/custom/chunks/0x028_spell.lua
@@ -0,0 +1,16 @@
+function ParseActionPacketSpell(ActionPacket)
+	local ActionID = ActionPacket["Param"]
+	local TargetID = ActionPacket["Target 1 ID"]
+
+	if ActionPacket["Target 1 Action 1 Has Added Effect"] then
+		local ActionMessage = ActionPacket["Target 1 Action 1 Added Effect Message"]
+		local Skillchain = _G[MAP_SKILLCHAIN_MESSAGES][ActionMessage]
+
+		if Skillchain then
+			CreateMagicBurstTracking(TargetID)
+		
+			local SkillchainElements = _G[MAP_SKILLCHAIN_ELEMENTS][Skillchain]
+			UpdateMagicBurstTracking(TargetID, SkillchainElements)
+		end
+	end
+end
---
+++ YAGDRSL/events/custom/chunks/0x028_weaponskill.lua
@@ -0,0 +1,24 @@
+function ParseActionPacketWeaponskill(ActionPacket)
+	local ActionID = ActionPacket["Param"]
+	local TargetID = ActionPacket["Target 1 ID"]
+	
+	if GetWeaponskillCanSkillchain(ActionID) then
+		if ActionPacket["Target 1 Action 1 Has Added Effect"] then
+			local ActionMessage = ActionPacket["Target 1 Action 1 Added Effect Message"]
+			local Skillchain = _G[MAP_SKILLCHAIN_MESSAGES][ActionMessage]
+
+			if Skillchain then
+				CreateMagicBurstTracking(TargetID)
+
+				local SkillchainElements = _G[MAP_SKILLCHAIN_ELEMENTS][Skillchain]
+				UpdateMagicBurstTracking(TargetID, SkillchainElements)
+			else
+				-- WS created an Added Effect but not a skillchain, a magic burst cannot occur.
+				DeleteMagicBurstTracking(TargetID)
+			end
+		else
+			-- WS has SC properties and did not create an Added Effect (skillchain), a magic burst cannot occur.
+			DeleteMagicBurstTracking(TargetID)
+		end
+	end
+end
