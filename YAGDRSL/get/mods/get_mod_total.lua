43f94ed06e939ba82c7339bbd9df001e2e0263cf
darkwaffle <darkewaffle@gmail.com>
2026-02-10 16:20:19 -0500

    Cleanup of legacy code

--- YAGDRSL/get/mods/get_mod_total.lua
+++ YAGDRSL/get/mods/get_mod_total.lua
@@ -1,31 +1,25 @@
 require "YAGDRSL/get/mods/get_mod_base.lua"
 
 function GetPrecastModOffense(SpellAttributes)
-	local BaseMod = GetPrecastModOffenseBase(SpellAttributes)
-	return set_combine(BaseMod, BuffMod)
+	return GetPrecastModOffenseBase(SpellAttributes)
 end
 
 function GetPrecastModDefense(SpellAttributes)
-	local BaseMod = GetPrecastModDefenseBase(SpellAttributes)
-	return set_combine(BaseMod, BuffMod)
+	return GetPrecastModDefenseBase(SpellAttributes)
 end
 
 function GetMidcastModOffense(SpellAttributes)
-	local BaseMod = GetMidcastModOffenseBase(SpellAttributes)
-	return set_combine(BaseMod, BuffMod)
+	return GetMidcastModOffenseBase(SpellAttributes)
 end
 
 function GetMidcastModDefense(SpellAttributes)
-	local BaseMod = GetMidcastModDefenseBase(SpellAttributes)
-	return set_combine(BaseMod, BuffMod)
+	return GetMidcastModDefenseBase(SpellAttributes)
 end
 
 function GetStatusModOffense(CharacterStatus)
-	local BaseMod = GetStatusModOffenseBase(CharacterStatus)
-	return set_combine(BaseMod, BuffMod)
+	return GetStatusModOffenseBase(CharacterStatus)
 end
 
 function GetStatusModDefense(CharacterStatus)
-	local BaseMod = GetStatusModDefenseBase(CharacterStatus)
-	return set_combine(BaseMod, BuffMod)
+	return GetStatusModDefenseBase(CharacterStatus)
 end
