85e4b45d6f172dcdeebaf0c101a018600041ec7d
darkwaffle <darkewaffle@gmail.com>
2026-02-12 17:48:30 -0500

    AftercastTerminate implemented to prevent aftercast equipping a status set when pet is midaction

--- YAGDRSL/events/gearswap/aftercast.lua
+++ YAGDRSL/events/gearswap/aftercast.lua
@@ -4,7 +4,15 @@ function aftercast(spell)
 
 	local SpellAttributes = GetSpellAttributes(spell)
 
-	AfterCastContainer(EVENT_AFTERCAST, SpellAttributes["IgnoreWeaponLock"])
+	TerminateStatus, TerminateReason = AftercastTerminate()
+	if TerminateStatus then
+		ChatWarning("aftercast Terminating: ", TerminateReason)
+		WriteDevLog("aftercast Terminating: ", TerminateReason)
+		return
+	else
+		AfterCastContainer(EVENT_AFTERCAST, SpellAttributes["IgnoreWeaponLock"])
+	end
+
 	HookOnAftercast(SpellAttributes) -- @Hook
 
 	ChatCheckpointLogged("Aftercast End", spell.name)
@@ -16,11 +24,33 @@ function AfterCastContainer(EventName, IgnoreWeaponLock)
 	local CharacterStatus = GetCharacterStatus()
 	ChatDebug(EventName .. ", CharacterStatus =", CharacterStatus)
 	local StatusSet = {}
-	StatusSet = GetStatusSet(CharacterStatus, EVENT_AFTERCAST)
+	StatusSet = GetStatusSet(CharacterStatus, EventName)
 
 	ChatGearSet(StatusSet, EventName)
 	EquipSafe(StatusSet, EventName, IgnoreWeaponLock)
 
+end
+
+function AftercastTerminate()
+	ChatCheckpointLogged("aftercast Termination Start")
+
+	local TerminateAftercast = false
+	local TerminateReason = ""
+
+	local TerminationFunctions =
+		{
+			TerminateUpdateOnPetMidaction
+		}
+
+	for _, TerminationCheck in ipairs(TerminationFunctions) do
+		TerminateAftercast, TerminateReason = TerminationCheck(EVENT_AFTERCAST)
+		if TerminateAftercast then
+			break
+		end
+	end
+
+	ChatCheckpointLogged("aftercast Termination End")
+	return TerminateAftercast, TerminateReason
 end
 
 function HookOnAftercast(SpellAttributes) -- @Hook
