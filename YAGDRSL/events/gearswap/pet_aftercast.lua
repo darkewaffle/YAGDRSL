f07f25fa6660796d6d45e75cd17ec734ec557bc7
darkwaffle <darkewaffle@gmail.com>
2026-02-12 17:48:56 -0500

    PetAftercastTerminate implemented to prevent pet_aftercast updating status set when player is midaction

--- YAGDRSL/events/gearswap/pet_aftercast.lua
+++ YAGDRSL/events/gearswap/pet_aftercast.lua
@@ -1,13 +1,47 @@
+require "YAGDRSL/events/gearswap/terminations/YAG_terminations.lua"
+
 function pet_aftercast(spell)
 	ChatBlankLine()
 	ChatCheckpointLogged("pet_aftercast Start")
 
-	AfterCastContainer(EVENT_AFTERCAST_PET)
+	local TerminateStatus = false
+	local TerminateReason = ""
+
+	TerminateStatus, TerminateReason = PetAftercastTerminate()
+	if TerminateStatus then
+		ChatWarning("pet_aftercast Terminating: ", TerminateReason)
+		WriteDevLog("pet_aftercast Terminating: ", TerminateReason)
+		return
+	else
+		AfterCastContainer(EVENT_AFTERCAST_PET)
+	end
 
 	local SpellAttributes = GetSpellAttributes(spell)
 	HookOnPetAftercast(SpellAttributes) -- @Hook
 
 	ChatCheckpointLogged("pet_aftercast End", spell.name)
+end
+
+function PetAftercastTerminate()
+	ChatCheckpointLogged("pet_aftercast Termination Start")
+
+	local TerminatePetAftercast = false
+	local TerminateReason = ""
+
+	local TerminationFunctions =
+		{
+			TerminateUpdateOnPlayerMidaction
+		}
+
+	for _, TerminationCheck in ipairs(TerminationFunctions) do
+		TerminatePetAftercast, TerminateReason = TerminationCheck(EVENT_AFTERCAST_PET)
+		if TerminatePetAftercast then
+			break
+		end
+	end
+
+	ChatCheckpointLogged("pet_aftercast Termination End")
+	return TerminatePetAftercast, TerminateReason
 end
 
 function HookOnPetAftercast(SpellAttributes) -- @Hook
