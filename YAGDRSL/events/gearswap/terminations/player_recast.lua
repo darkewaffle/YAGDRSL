1877feadddb49acf9c61f00bc6d63d7c0deacec2
darkwaffle <darkewaffle@gmail.com>
2026-02-12 18:10:15 -0500

    Temporary workaround so Recast termination does not incorrectly cancel Stratagems and Ready

--- YAGDRSL/events/gearswap/terminations/player_recast.lua
+++ YAGDRSL/events/gearswap/terminations/player_recast.lua
@@ -3,7 +3,10 @@ function TerminateSpellOnRecast(SpellAttributes)
 	local TerminateReason = ""
 
 	if SpellAttributes["Category"] == CATEGORY_JA then
-		if GetRecastJobAbility(SpellAttributes["RecastID"]) > 0 then
+		local JARecast = GetRecastJobAbility(SpellAttributes["RecastID"])
+
+		-- 255 = ready, 233 = stratagems, 231 = stratagem abilities. Temporary fix until implementation of 'get charges' logic.
+		if GetRecastJobAbility(SpellAttributes["RecastID"]) > 0 and SpellAttributes["RecastID"] ~= 255 and SpellAttributes["RecastID"] ~= 233 and SpellAttributes["RecastID"] ~= 231 then
 			TerminateSpell = true
 			TerminateReason = "Job ability is still on recast = " .. SpellAttributes["Name"]
 		end
