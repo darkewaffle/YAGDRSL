7e4c3246c7ebc3ea46229c3a6ba3e3d3c9b665e7
darkwaffle <darkewaffle@gmail.com>
2026-02-15 17:12:00 -0500

    Collection armor all now has priority = HP granted by the item

--- YAGDRSL/declare/tables/collections/declare_collections.lua
+++ YAGDRSL/declare/tables/collections/declare_collections.lua
@@ -14,81 +14,81 @@ function DeclareCollectionsTable()
 
 	_G[COLLECTIONS_SETS_ROOT_NAME]["Agwu"] =
 		{
-		head="Agwu's Cap",
-		body="Agwu's Robe",
-		hands="Agwu's Gages",
-		legs="Agwu's Slops",
-		feet="Agwu's Pigaches"
+		head={name="Agwu's Cap", priority=38},
+		body={name="Agwu's Robe", priority=61},
+		hands={name="Agwu's Gages", priority=38},
+		legs={name="Agwu's Slops", priority=50},
+		feet={name="Agwu's Pigaches", priority=27}
 		}
 
 	_G[COLLECTIONS_SETS_ROOT_NAME]["Bunzi"] =
 		{
-		head="Bunzi's Hat",
-		body="Bunzi's Robe",
-		hands="Bunzi's Gloves",
-		legs="Bunzi's Pants",
-		feet="Bunzi's Sabots"
+		head={name="Bunzi's Hat", priority=50},
+		body={name="Bunzi's Robe", priority=72},
+		hands={name="Bunzi's Gloves", priority=50},
+		legs={name="Bunzi's Pants", priority=61},
+		feet={name="Bunzi's Sabots", priority=38}
 		}
 
 	_G[COLLECTIONS_SETS_ROOT_NAME]["Gleti"] =
 		{
-		head="Gleti's Mask",
-		body="Gleti's Cuirass",
-		hands="Gleti's Gauntlets",
-		legs="Gleti's Breeches",
-		feet="Gleti's Boots"
+		head={name="Gleti's Mask", priority=68},
+		body={name="Gleti's Cuirass", priority=91},
+		hands={name="Gleti's Gauntlets", priority=68},
+		legs={name="Gleti's Breeches", priority=79},
+		feet={name="Gleti's Boots", priority=57}
 		}
 
 	_G[COLLECTIONS_SETS_ROOT_NAME]["Ikenga"] =
 		{
-		head="Ikenga's Hat",
-		body="Ikenga's Vest",
-		hands="Ikenga's Gloves",
-		legs="Ikenga's Trousers",
-		feet="Ikenga's Clogs"
+		head={name="Ikenga's Hat", priority=57},
+		body={name="Ikenga's Vest", priority=79},
+		hands={name="Ikenga's Gloves", priority=57},
+		legs={name="Ikenga's Trousers", priority=68},
+		feet={name="Ikenga's Clogs", priority=45}
 		}
 
 	_G[COLLECTIONS_SETS_ROOT_NAME]["Malignance"] =
 		{
-		head="Malignance Chapeau",
-		body="Malignance Tabard",
-		hands="Malignance Gloves",
-		legs="Malignance Tights",
-		feet="Malignance Boots"
+		head={name="Malignance Chapeau", priority=45},
+		body={name="Malignance Tabard", priority=68},
+		hands={name="Malignance Gloves", priority=57},
+		legs={name="Malignance Tights", priority=45},
+		feet={name="Malignance Boots", priority=34}
 		}
 
 	_G[COLLECTIONS_SETS_ROOT_NAME]["Mpaca"] =
 		{
-		head="Mpaca's Cap",
-		body="Mpaca's Doublet",
-		hands="Mpaca's Gloves",
-		legs="Mpaca's Hose",
-		feet="Mpaca's Boots"
+		head={name="Mpaca's Cap", priority=61},
+		body={name="Mpaca's Doublet", priority=84},
+		hands={name="Mpaca's Gloves", priority=61},
+		legs={name="Mpaca's Hose", priority=72},
+		feet={name="Mpaca's Boots", priority=50}
 		}
 
 	_G[COLLECTIONS_SETS_ROOT_NAME]["Null"] =
 		{
-		head="Null Masque",
-		neck="Null Loop",
-		waist="Null Belt",
-		back="Null Shawl"
+		head={name="Null Masque", priority=100},
+		neck={name="Null Loop", priority=50},
+		waist={name="Null Belt", priority=0},
+		back={name="Null Shawl", priority=0}
 		}
 
 	_G[COLLECTIONS_SETS_ROOT_NAME]["Nyame"] =
 		{
-		head="Nyame Helm",
-		body="Nyame Mail",
-		hands="Nyame Gauntlets",
-		legs="Nyame Flanchard",
-		feet="Nyame Sollerets"
+		head={name="Nyame Helm", priority=91},
+		body={name="Nyame Mail", priority=136},
+		hands={name="Nyame Gauntlets", priority=91},
+		legs={name="Nyame Flanchard", priority=114},
+		feet={name="Nyame Sollerets", priority=68}
 		}
 
 	_G[COLLECTIONS_SETS_ROOT_NAME]["Sakpata"] =
 		{
-		head="Sakpata's Helm",
-		body="Sakpata's Breastplate",
-		hands="Sakpata's Gauntlets",
-		legs="Sakpata's Cuisses",
-		feet="Sakpata's Leggings"
+		head={name="Sakpata's Helm", priority=91},
+		body={name="Sakpata's Breastplate", priority=136},
+		hands={name="Sakpata's Gauntlets", priority=91},
+		legs={name="Sakpata's Cuisses", priority=114},
+		feet={name="Sakpata's Leggings", priority=68}
 		}
 end
