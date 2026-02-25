4cf24d5946e7b3d98d2b9b11c165c4b3dfbca983
darkwaffle <darkewaffle@gmail.com>
2026-02-24 21:23:03 -0500

    Pet tracking now supports a 'parent' value for avatars and automatons. x.y.avatar["Ramuh"] or a.b.automaton["ve_ve"] for example.

--- YAGDRSL/do/track_pet.lua
+++ YAGDRSL/do/track_pet.lua
@@ -27,11 +27,12 @@ function UpdatePetNameTracker(NewPet)
 		PetName = TRACK_PET_LUOPAN
 
 	elseif CharacterJob == "PUP" then
-		PetName = TRACK_PET_AUTOMATON
+		PetName = ConstructAutomatonType()
+		SetPetTrackerParent(TRACK_PET_AUTOMATON)
 
 	elseif CharacterJob == "SMN" then
 		PetName = NewPet
---		SetPetTrackerParent("avatar")
+		SetPetTrackerParent("avatar")
 	
 	else
 		PetName = MOD_DEFAULT_OFF
@@ -54,10 +55,10 @@ function TrackPetNameAsDefenseMod()
 	TrackPetNameAsMod(MOD_CLASS_DEFENSE)
 end
 
---function SetPetTrackerParent(ParentName)
---	local PetTrackerName = FormatTrackerName(TRACK_PET_NAME)
---	SetModParent(PetTrackerName, ParentName)
---end
+function SetPetTrackerParent(ParentName)
+	local PetTrackerName = FormatTrackerName(TRACK_PET_NAME)
+	SetModParent(PetTrackerName, ParentName)
+end
 
 function ConstructAutomatonType()
 	local PupData = windower.ffxi.get_mjob_data()
