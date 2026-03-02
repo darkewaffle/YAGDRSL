9919e4a26e2a1b716c2a9d5143ef7a63e1feb940
darkwaffle <darkewaffle@gmail.com>
2026-03-01 19:05:44 -0500

    Implemented simple logging to a file on disk to write more robust error messages and debug information

--- YAGDRSL/debug/YAG_debug.lua
+++ YAGDRSL/debug/YAG_debug.lua
@@ -1,3 +1,4 @@
 require "YAGDRSL/debug/chat.lua"
 require "YAGDRSL/debug/chat_objects.lua"
+require "YAGDRSL/debug/log_file.lua"
 require "YAGDRSL/debug/performance.lua"
---
+++ YAGDRSL/debug/log_file.lua
@@ -0,0 +1,32 @@
+require "YAGDRSL/do/modify_strings.lua"
+
+local LogFilePath = windower.addon_path .. "data/export/YAG_log.txt"
+local LogSequence = 1
+
+function AddToLogFile(Reason, Message)
+	local SequenceLabel = tostring(LogSequence) .. " "
+	local LogStart = SequenceLabel .. "START = " .. os.date("%c") .. " @" .. os.clock() .. "\n"
+	local LogReason = SequenceLabel .. "REASON = " .. tostring(Reason) .. "\n"
+	local LogMessage = ""
+
+	if type(Message) == "table" then
+		for Key, Value in pairs(Message) do
+			local KeyString = SetStringWidth(Key, 25)
+			local ValueString = tostring(Value)
+			LogMessage = LogMessage .. SequenceLabel .. "KEY=" .. KeyString .. "   " .. "VALUE=" .. ValueString .. "\n"
+		end
+	else
+		LogMessage = SequenceLabel .. tostring(Message) .. "\n"
+	end
+
+	local LogFile = io.open(LogFilePath, "a+")
+	LogFile:write(LogStart)
+	LogFile:write(LogReason)
+	LogFile:write(LogMessage)
+
+	local LogEnd = SequenceLabel .. "END = " .. os.date("%c") .. " @" .. os.clock() .. "\n\n"
+	LogFile:write(LogEnd)
+	LogFile:close()
+
+	LogSequence = LogSequence + 1
+end
