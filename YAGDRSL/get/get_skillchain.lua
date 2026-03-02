dfcec87d2abcd4f0462f1e2f60441abd65542ead
darkwaffle <darkewaffle@gmail.com>
2026-03-01 19:13:42 -0500

    Improved handling of unexpected inputs and added logging to diagnose

--- YAGDRSL/get/get_skillchain.lua
+++ YAGDRSL/get/get_skillchain.lua
@@ -1,26 +1,44 @@
 function GetWeaponskillCanSkillchain(WeaponskillID)
-	local FirstProperty = WINDOWER_RESOURCES.weapon_skills[WeaponskillID].skillchain_a
-	if FirstProperty and FirstProperty ~= "" then
-		return true
+	if WeaponskillID and WINDOWER_RESOURCES.weapon_skills[WeaponskillID] then
+		local FirstProperty = WINDOWER_RESOURCES.weapon_skills[WeaponskillID].skillchain_a
+		if FirstProperty and FirstProperty ~= "" then
+			return true
+		else
+			return false
+		end
 	else
+		ChatError("Unknown WeaponskillID", WeaponskillID)
+		AddToLogFile("Unknown WeaponskillID", WeaponskillID)
 		return false
 	end
 end
 
 function GetJobAbilityCanSkillchain(JobAbilityID)
-	local FirstProperty = WINDOWER_RESOURCES.job_abilities[JobAbilityID].skillchain_a
-	if FirstProperty and FirstProperty ~= "" then
-		return true
+	if JobAbilityID and WINDOWER_RESOURCES.job_abilities[JobAbilityID] then
+		local FirstProperty = WINDOWER_RESOURCES.job_abilities[JobAbilityID].skillchain_a
+		if FirstProperty and FirstProperty ~= "" then
+			return true
+		else
+			return false
+		end
 	else
+		ChatError("Unknown JobAbilityID", JobAbilityID)
+		AddToLogFile("Unknown JobAbilityID", JobAbilityID)
 		return false
 	end
 end
 
 function GetNPCAbilityCanSkillchain(AbilityID)
-	local FirstProperty = WINDOWER_RESOURCES.monster_abilities[AbilityID].skillchain_a
-	if FirstProperty and FirstProperty ~= "" then
-		return true
+	if AbilityID and WINDOWER_RESOURCES.monster_abilities[AbilityID] then
+		local FirstProperty = WINDOWER_RESOURCES.monster_abilities[AbilityID].skillchain_a
+		if FirstProperty and FirstProperty ~= "" then
+			return true
+		else
+			return false
+		end
 	else
+		ChatError("Unknown NPC AbilityID", AbilityID)
+		AddToLogFile("Unknown NPC AbilityID", AbilityID)
 		return false
 	end
 end
