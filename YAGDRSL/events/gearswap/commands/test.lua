bc9bc31a6f24ca5df3dc1a5384082b3669717cb0
darkwaffle <darkewaffle@gmail.com>
2026-05-30 20:41:27 -0400

    test1 command to provide debug information on Warp Ring reuse

--- YAGDRSL/events/gearswap/commands/test.lua
+++ YAGDRSL/events/gearswap/commands/test.lua
@@ -1,6 +1,36 @@
 function SelfCommandTest1()
 	ChatCheckpoint("Command Test 1 Start")
 
+	local ExtDataLibrary = require "extdata"
+
+	local WarpRingID = 28540
+	local WarpRingItem = {}
+	local WarpRingStatus = {}
+
+	local SearchContainers =
+		{
+			-- Inventory and all 8 wardrobes
+			0, 8, 10, 11, 12, 13, 14, 15, 16
+		}
+
+	-- Iterate through inventory and wardrobes, get the items and if they are armor then put them into ArmorItems.
+	for _, ContainerIndex in pairs(SearchContainers) do
+
+		local ContainerItems = windower.ffxi.get_items(ContainerIndex)
+		for _, Item in ipairs(ContainerItems) do
+			if Item.id == WarpRingID then
+				WarpRingItem = Item
+				break
+			end
+		end
+	end
+
+	if WarpRingItem.extdata then
+		WarpRingStatus = ExtDataLibrary.decode(WarpRingItem)
+		ChatError("Warp Ring Reuse Time", WarpRingStatus["next_use_time"])
+		ChatError("Warp Ring Reuse Date", os.date("%c", WarpRingStatus["next_use_time"]))
+	end
+
 	ChatCheckpoint("Command Test 1 End")
 end
 
