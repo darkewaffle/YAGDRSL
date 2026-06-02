210b07a28aa3c0b6209fca025d6e2b0273a2a399
darkwaffle <darkewaffle@gmail.com>
2026-06-01 21:28:56 -0400

    Renamed all sample script files and updated all references to script files so that they are expected to be .txt rather than .lua. I don't know why I thought they were .lua files at some point. Live and learn.

--- YAGDRSL/do/format_script_name.lua
+++ YAGDRSL/do/format_script_name.lua
@@ -1,8 +1,8 @@
 function FormatScriptName(ScriptName)
 	if ScriptName and ScriptName ~= "" then
-		-- If the last 4 characters of ScriptName are not ".lua" then append ".lua" to the end.
-		if string.sub(ScriptName, -4, -1) ~= ".lua" then
-			ScriptName = ScriptName .. ".lua"
+		-- If the last 4 characters of ScriptName are not ".txt" then append ".txt" to the end.
+		if string.sub(ScriptName, -4, -1) ~= ".txt" then
+			ScriptName = ScriptName .. ".txt"
 		end
 	end
 	return ScriptName
