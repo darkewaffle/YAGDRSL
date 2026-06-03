000b14cef3456f3b83338784eb433a96bdba6665
darkwaffle <darkewaffle@gmail.com>
2026-06-03 12:43:03 -0400

    Default lockstyle set to 0 in character template, when lockstyle = 0 then SetJobLockStyle will not send the lockstyle command. This helps let users manage their lockstyle with other addons if they prefer.

--- YAGDRSL/do/set_job_lockstyle.lua
+++ YAGDRSL/do/set_job_lockstyle.lua
@@ -2,8 +2,13 @@ require "YAGDRSL/debug/chat.lua"
 require "YAGDRSL/get/get_jobcontrol.lua"
 
 function SetJobLockStyle()
-	local LockStyleIndex = tostring(GetMainJobLockStyle())
-	ChatDebug("Set LockStyle", LockStyleIndex)
-	send_command("input /lockstyleset " .. LockStyleIndex)
+	local LockStyleIndex = GetMainJobLockStyle()
+
+	if LockStyleIndex ~= 0 then
+		LockStyleIndex = tostring(LockStyleIndex)
+		ChatDebug("Set LockStyle", LockStyleIndex)
+		send_command("input /lockstyleset " .. LockStyleIndex)
+	end
+
 	STATE_SET_LOCKSTYLE_PENDING = false
 end
