68513fea012ea5d9e40cc2966a085589174407ad
darkwaffle <darkewaffle@gmail.com>
2026-06-07 20:52:59 -0400

    Implementation of AssignOverrideOptions and SetOverrideValue to allow Override to be manipulated in the same way as other mods.

--- YAGDRSL/do/create_override.lua
+++ YAGDRSL/do/create_override.lua
@@ -4,3 +4,12 @@ require "YAGDRSL/do/create_mod.lua"
 function ConfigureOverride(OverrideDescription, ControlPanelDisplayOrder, OverrideKeyBind, ...)
 	CreateMod(OVERRIDE_VALUES_ROOT_NAME, OverrideDescription, ControlPanelDisplayOrder, OverrideKeyBind, ...)
 end
+
+
+function AssignOverrideOptions(...)
+	AssignModOptions(OVERRIDE_VALUES_ROOT_NAME, ...)
+end
+
+function SetOverrideValue(Value)
+	SetModValue(OVERRIDE_VALUES_ROOT_NAME, Value)
+end
