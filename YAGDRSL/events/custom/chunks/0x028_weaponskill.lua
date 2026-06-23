174ef563ff9a87fadb946affd1bc37809661cf78
darkwaffle <darkewaffle@gmail.com>
2026-06-23 18:21:48 -0400

    Improved handling for action packets to track magicburst state. Updated logic to account for NPC actions being sent as weaponskill packets but containing IDs that correspond to the monster_abilities resource.

--- YAGDRSL/events/custom/chunks/0x028_weaponskill.lua
+++ YAGDRSL/events/custom/chunks/0x028_weaponskill.lua
@@ -2,7 +2,21 @@ function ParseActionPacketWeaponskill(ActionPacket)
 	local ActionID = ActionPacket["Param"]
 	local TargetID = ActionPacket["Target 1 ID"]
 	
-	if GetWeaponskillCanSkillchain(ActionID) then
+	local CanSkillchain = false
+
+	-- <= 255 are normal weaponskills
+	if ActionID <= 255 then
+		CanSkillchain = GetWeaponskillCanSkillchain(ActionID)
+
+	-- >= 257 do not exist in the WS resource. They are NPC/monster abilities found in monster_abilities.lua.
+	-- Trust weaponskills are [sometimes?] received with a category of 3 (weaponskill finish) rather than 11 (NPC TP finish)
+	-- However they will contain an action ID that corresponds to a monster ability rather than a normal weaponskill
+	-- For example - Sylvie (UC) will use Nott which has an ID of 3502 in monster_abilities.lua
+	elseif ActionID >= 257 then
+		CanSkillchain = GetNPCAbilityCanSkillchain(ActionID)
+	end
+
+	if CanSkillchain then
 		if ActionPacket["Target 1 Action 1 Has Added Effect"] then
 			local ActionMessage = ActionPacket["Target 1 Action 1 Added Effect Message"]
 			local Skillchain = _G[MAP_SKILLCHAIN_MESSAGES][ActionMessage]
